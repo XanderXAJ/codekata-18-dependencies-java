c9938cf3128820bdb7377385badf10562607f570
Alex Palmer <xanderx@xanderx.com>
2019-08-16 20:18:28 +0100

    Add test for graph with no transitive dependencies

--- src/test/java/codekata/dependencies/TransitiveDependencyGraphResolverTest.java
+++ src/test/java/codekata/dependencies/TransitiveDependencyGraphResolverTest.java
@@ -3,6 +3,9 @@ package codekata.dependencies;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +16,15 @@ class TransitiveDependencyGraphResolverTest {
 	@Test
 	void resolvesNothingAsItself() {
 		DependencyGraph graph = new DependencyGraph(Collections.emptyMap());
+
+		assertThat(resolver.resolve(graph)).isEqualTo(graph);
+	}
+
+	@Test
+	void resolvesGraphWithNoTransitiveDependenciesAsItself() {
+		Map<String, Set<String>> dependencyMap = new HashMap<>();
+		dependencyMap.put("A", Set.of("B"));
+		DependencyGraph graph = new DependencyGraph(dependencyMap);
 
 		assertThat(resolver.resolve(graph)).isEqualTo(graph);
 	}
